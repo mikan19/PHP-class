83f95282617ee5ecb8b7d31fca601d76450a31ac
mikan19 <mellamo.rina.ty@gmail.com>
2023-05-02 21:50:29 +0900

    fix:数値が出力されるよう修正しました

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,36 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$distance = new Distance(5);
+$time = new Time(4);
+
+
+echo $distance->distance() / $time->time();
+ 
+class Distance
+{
+    private $distance;
+ 
+    public function __construct(int $distance)
+    {
+        $this->distance = $distance;
+    }
+
+    public function distance()
+    {
+        return $this->distance;
+    }
+}
+
+class Time
+{
+    private $time;
+    
+    public function __construct(int $time)
+    {
+        $this->time = $time;
+    }
+    
+    public function time()
+    {
+        return $this->time;
+    }
+}
